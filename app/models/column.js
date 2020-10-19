e1f5d80b7f437fd18ebbbb032f10ebd4ba802aff
Herbert Hudson <herberthudson@gmail.com>
2020-10-19 20:40:56 -0300

    creating column model

---
+++ app/models/column.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose")
+
+const columnSchema = new mongoose.Schema(
+  {
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Table",
+      required: true,
+    },
+    //TODO: implement card model
+    // cards: [card.schema],
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    // Date createdAt
+    timestamps: true,
+  }
+)
+
+module.exports = mongoose.model("Column", columnSchema)
